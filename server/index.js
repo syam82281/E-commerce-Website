eb3c7884876718d77a3e068ba5d67c9429141591
root <root@ip-172-31-16-13.us-east-2.compute.internal>
2026-02-08 20:02:32 +0000

    Added docker setup + backend + frontend

--- server/index.js
+++ server/index.js
@@ -23,9 +23,9 @@ app.get('/',(req,res)=>{
 
 app.use('/api/v1',paymentRoutes);
 
-const PORT=process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
+app.listen(PORT,"0.0.0.0",()=>{
     console.log(`Server is running at port ${PORT}`)
 });
 
